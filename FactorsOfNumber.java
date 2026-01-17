7da6751dcb9595d42cef9ee08fddca75707ee6c6
pravin-rathod-git <pr819270@gmail.com>
2026-01-17 23:57:00 +0530

    find out the factors of the number

---
+++ FactorsOfNumber.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class FactorsOfNumber {
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+
+        int n=s.nextInt();
+
+        for(int i=1;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                System.out.println(i+" ");
+                if(i!=n/i){
+                    System.out.println(n/i);
+                }
+            }
+        }
+    }
+}
